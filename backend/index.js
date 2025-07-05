4f513f2963923b0c40b8aaf58b89873c428b807a
yoyokorone <sangeeta.saxena6724@gmail.com>
2025-07-05 14:36:19 +0530

    firstyy

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,9 @@ app.use(express.json());
 app.use(
 	cors({
 		origin:'*',
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	})
 ); 
-
-app.options('*', cors());
 
 app.use(express.urlencoded({ extended: true }));
 
