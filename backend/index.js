f764f91d6336851df146b7cd090e078557940833
yoyokorone <sangeeta.saxena6724@gmail.com>
2025-07-06 20:39:15 +0530

    firstyy

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: 'https://arthacuisine.netlify.app',
 		credentials: true,
 	})
 );
