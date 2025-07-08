3c9f509859417da307c90a442ebfba41dd6fd415
yoyokorone <sangeeta.saxena6724@gmail.com>
2025-07-08 14:38:09 +0530

    firstyy

--- src/redux/Utils/constants.js
+++ src/redux/Utils/constants.js
@@ -1,32 +1,32 @@
 export const BASE_URL = 'https://arthacuisine.netlify.app/.netlify/functions/'
 
-export const LOGIN_API = 'https://acpl.onrender.com/api/users/login'
-export const REGISTER_API = 'https://acpl.onrender.com/api/users/signup'
-export const LOGOUT_API = 'https://acpl.onrender.com/api/users/logout'
+export const LOGIN_API = 'https://arthacuisine.netlify.app/.netlify/functions/users/login'
+export const REGISTER_API = 'https://arthacuisine.netlify.app/.netlify/functions/users/signup'
+export const LOGOUT_API = 'https://arthacuisine.netlify.app/.netlify/functions/users/logout'
 
-export const GOOGLE_API = 'https://acpl.onrender.com/api/users/googleLogin'
+export const GOOGLE_API = 'https://arthacuisine.netlify.app/.netlify/functions/users/googleLogin'
 export const CLIENT_ID = "922549263546-0jat4nh1jrjt07a58nhsm469vdsvhdbb.apps.googleusercontent.com"
 
-export const OTPLOG_URL = 'https://acpl.onrender.com/api/users/otpLogin'
-export const GENOTP_URL = 'https://acpl.onrender.com/api/users/otpEntry'
-export const VERIFY_URL = 'https://acpl.onrender.com/users/verifyOtp'
+export const OTPLOG_URL = 'https://arthacuisine.netlify.app/.netlify/functions/users/otpLogin'
+export const GENOTP_URL = 'https://arthacuisine.netlify.app/.netlify/functions/users/otpEntry'
+export const VERIFY_URL = 'https://arthacuisine.netlify.app/.netlify/functions/users/verifyOtp'
 
 export const PROFILE_URL = 'https://arthacuisine.netlify.app/.netlify/functions/profile'
 export const USERINFO_URL = 'https://arthacuisine.netlify.app/.netlify/functions/getAllUsers'
-export const USERID_URL = 'https://arthacuisine.netlify.app/.netlify/functions/api/users/'
+export const USERID_URL = 'https://arthacuisine.netlify.app/.netlify/functions/users/'
 
 export const CATEGORIES_URL = 'https://arthacuisine.netlify.app/.netlify/functions/category'
 
-export const PRODUCT_URL = 'https://acpl.onrender.com/api/product'
+export const PRODUCT_URL = 'https://arthacuisine.netlify.app/.netlify/functions/product'
 
-export const GOAL_URL = 'https://acpl.onrender.com/api/goal'
+export const GOAL_URL = 'https://arthacuisine.netlify.app/.netlify/functions/goal'
 
-export const DATA_URL = 'https://acpl.onrender.com/api/data'
+export const DATA_URL = 'https://arthacuisine.netlify.app/.netlify/functions/data'
 
-export const UPLOAD_URL = 'https://acpl.onrender.com/api/upload'
+export const UPLOAD_URL = 'https://arthacuisine.netlify.app/.netlify/functions/upload'
 
-export const DISPATCH_URL = 'https://acpl.onrender.com/api/dispatch'
+export const DISPATCH_URL = 'https://arthacuisine.netlify.app/.netlify/functions/dispatch'
 
-export const EXPORT_URL = 'https://acpl.onrender.com/api/export'
+export const EXPORT_URL = 'https://arthacuisine.netlify.app/.netlify/functions/export'
 
 export const GRAPH_URL = 'https://arthacuisine.netlify.app/.netlify/functions/graph'
