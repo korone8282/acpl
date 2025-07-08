7416a1adced1e0502778042e5298106715341dc5
yoyokorone <sangeeta.saxena6724@gmail.com>
2025-07-08 18:00:54 +0530

    firstyy

--- src/redux/Utils/constants.js
+++ src/redux/Utils/constants.js
@@ -1,32 +1,34 @@
-export const BASE_URL = 'https://acpl.onrender.com/'
+export const BASE_URL = 'https://acpl.onrender.com'
 
-export const LOGIN_API = 'https://acpl.onrender.com/users/login'
-export const REGISTER_API = 'https://acpl.onrender.com/users/signup'
-export const LOGOUT_API = 'https://acpl.onrender.com/users/logout'
+export const LOGIN_API = 'https://acpl.onrender.com/api/users/login'
+export const REGISTER_API = 'https://acpl.onrender.com/api/users/signup'
+export const LOGOUT_API = 'https://acpl.onrender.com/api/users/logout'
 
-export const GOOGLE_API = 'https://acpl.onrender.com/users/googleLogin'
+export const GOOGLE_API = 'https://acpl.onrender.com/api/users/googleLogin'
 export const CLIENT_ID = "922549263546-0jat4nh1jrjt07a58nhsm469vdsvhdbb.apps.googleusercontent.com"
 
-export const OTPLOG_URL = 'https://acpl.onrender.com/users/otpLogin'
-export const GENOTP_URL = 'https://acpl.onrender.com/users/otpEntry'
-export const VERIFY_URL = 'https://acpl.onrender.com/users/verifyOtp'
+export const OTPLOG_URL = 'https://acpl.onrender.com/api/users/otpLogin'
+export const GENOTP_URL = 'https://acpl.onrender.com/api/users/otpEntry'
+export const VERIFY_URL = 'https://acpl.onrender.com/api/users/verifyOtp'
 
-export const PROFILE_URL = 'https://acpl.onrender.com/profile'
-export const USERINFO_URL = 'https://acpl.onrender.com/getAllUsers'
-export const USERID_URL = 'https://acpl.onrender.com/users/'
+export const PROFILE_URL = 'https://acpl.onrender.com/api/users/profile'
+export const USERINFO_URL = 'https://acpl.onrender.com/api/users/getAllUsers'
+export const USERID_URL = 'https://acpl.onrender.com/api/users/'
 
-export const CATEGORIES_URL = 'https://acpl.onrender.com/category'
+export const UPLOAD_URL = 'https://acpl.onrender.com/api/uploads'
 
-export const PRODUCT_URL = 'https://acpl.onrender.com/product'
+export const CATEGORIES_URL = 'https://acpl.onrender.com/api/category'
 
-export const GOAL_URL = 'https://acpl.onrender.com/goal'
+export const PRODUCT_URL = 'https://acpl.onrender.com/api/product'
 
-export const DATA_URL = 'https://acpl.onrender.com/data'
+export const GOAL_URL = 'https://acpl.onrender.com/api/goal'
 
-export const UPLOAD_URL = 'https://acpl.onrender.com/upload'
+export const DATA_URL = 'https://acpl.onrender.com/api/data'
 
-export const DISPATCH_URL = 'https://acpl.onrender.com/dispatch'
+export const STORE_URL = 'https://acpl.onrender.com/api/inventory'
 
-export const EXPORT_URL = 'https://acpl.onrender.com/export'
+export const DISPATCH_URL = 'https://acpl.onrender.com/api/dispatch'
 
-export const GRAPH_URL = 'https://acpl.onrender.com/graph'
+export const EXPORT_URL = 'https://acpl.onrender.com/api/export'
+
+export const GRAPH_URL = 'https://acpl.onrender.com/api/graph'
